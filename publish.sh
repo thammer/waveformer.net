dc0cb37477b035a05cfd6d1b3ef0e72dee210331
Thomas Hammer <h@mmer.no>
2020-07-14 18:19:55 +0200

    Get latest changes for submodules

--- publish.sh
+++ publish.sh
@@ -6,8 +6,10 @@ if [ "$#" -ne 1 ]; then
 fi
 output=$1
 
+echo "Getting the latest submodules (and deleting all local changes to submodules)"
+git submodule foreach git reset --hard
+git submodule update --recursive --remote
 echo "Publishing to directory \"$output\""
-rsync -avr --exclude='.git' --exclude='.vscode' --exclude='*.sh' . $output
+sed -i -e "s/<\!-- For www.waveformer.net //" -e "s/-->//" midimonitor/index.html
+rsync -avr --exclude='.git' --exclude='.vscode' --exclude='*.sh' --exclude=".gitmodules" . $output
 
-mkdir -p $output/midimonitor
-cp ../midimonitor/index.html $output/midimonitor 
