0bcf66d9b2e5ff38c296c3c73cbbeec303402a0b
Thomas Hammer <h@mmer.no>
2020-07-25 18:59:26 +0200

    Added circuitbender submodule

--- publish.sh
+++ publish.sh
@@ -9,7 +9,9 @@ output=$1
 echo "Getting the latest submodules (and deleting all local changes to submodules)"
 git submodule foreach git reset --hard
 git submodule update --recursive --remote
-echo "Publishing to directory \"$output\""
+echo "Patching submodules (adding navbar)"
 sed -i -e "s/<\!-- For www.waveformer.net //" -e "s/-->//" midimonitor/index.html
+sed -i -e "s/<\!-- For www.waveformer.net //" -e "s/-->//" circuitbender/index.html
+echo "Publishing to directory \"$output\""
 rsync -avr --exclude='.git' --exclude='.vscode' --exclude='*.sh' --exclude=".gitmodules" . $output
 
